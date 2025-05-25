592771083b252c84896560d1a9334157767febf6
Dorian <168729991+DorianA37@users.noreply.github.com>
2025-05-25 13:08:18 +0200

    création de la connexion à la bdd

--- QuestionCRUD.cs
+++ QuestionCRUD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace Projet_AskMeIfYouCan
 {
     public class QuestionCRUD
     {
+        private SQLiteConnection connection;
+
+        /// <summary>
+        /// Initialise une nouvelle instance de la classe QuestionCRUD avec le nom de la base de données
+        /// </summary>
+        /// <param name="nomBdd">nom de la base de données</param>
+        public QuestionCRUD(string nomBdd)
+        {
+            connection = new SQLiteConnection("Data Source=" + nomBdd + ".sqlite;Version=3;");
+            connection.Open();
+        }
     }
 }
--- ReponseCRUD.cs
+++ ReponseCRUD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace Projet_AskMeIfYouCan
 {
     internal class ReponseCRUD
     {
+        private SQLiteConnection connection;
+
+        /// <summary>
+        /// Initialise une nouvelle instance de la classe ReponseCRUD avec le nom de la base de données
+        /// </summary>
+        /// <param name="nomBdd">nom de la base de données</param>
+        public ReponseCRUD(string nomBdd)
+        {
+            connection = new SQLiteConnection("Data Source=" + nomBdd + ".sqlite;Version=3;");
+            connection.Open();
+        }
     }
 }
